a0f5eabc837d20d37f1dc73eb321417abbd6bdc9
Kyle Watkins <Kyle@MakBook.local>
2016-12-06 14:44:16 -0600

    Main Screen GUI
    Preston and I started the design of the home screen on the app.

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -1,12 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.DefaultListModel;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /*
  * Team 6: Coffee Beans
@@ -17,18 +24,80 @@ import javax.swing.JScrollPane;
 
 public class CoffeeShop extends JFrame implements ActionListener{
 
-	JPanel mainScreen = new JPanel();
-	DefaultListModel data = new DefaultListModel();
-	JList orderList = new JList(data);
-	JScrollPane jsp = new JScrollPane(orderList);
-	JButton checkout = new JButton("Check out");
+	JPanel eastPanel = new JPanel();
+	JPanel buttonPanel = new JPanel();
+	JPanel mainPanel = new JPanel();
+
+	JButton checkoutButton = new JButton("Check Out");
+	JButton editItem = new JButton("Edit Selected Item");
+	JButton removeItem = new JButton("Remove Selected Item");
+	
+	
+	JLabel orderTotalLbl = new JLabel("$0.00");
+	
+	DefaultTableModel model;
+	JTable checkoutTable;
+	JScrollPane sp;
 	
 	public CoffeeShop() {
-		checkout.addActionListener(this);
-		mainScreen.add(jsp);
-		mainScreen.add(checkout);
-		add(mainScreen);
 		
+		// Table Settings
+		//--------------------------------------------------
+		model = new DefaultTableModel(0, 2) {
+			public boolean isCellEditable(int row, int column) {
+				return false; // makes table cells not-editable
+			}
+		};
+
+		checkoutTable = new JTable(model);
+		sp = new JScrollPane(checkoutTable);
+		
+		Object[] columns = { "      Order Item" , "  Price" };
+		model.setColumnIdentifiers(columns);
+
+		checkoutTable.setGridColor(Color.BLACK);// colors grid lines
+		
+		checkoutTable.getTableHeader().setFont(new Font("Arial",Font.PLAIN, 25));
+		checkoutTable.getColumnModel().getColumn(0).setPreferredWidth(220);
+		checkoutTable.getColumnModel().getColumn(1).setPreferredWidth(100);
+		
+		//--------------------------------------------------
+		
+		
+		// fancy order total $$ label
+		orderTotalLbl.setFont(new Font("Courier", Font.PLAIN, 40));
+		orderTotalLbl.setForeground(new Color(128, 64, 0));
+		orderTotalLbl.setBackground(Color.WHITE);
+		orderTotalLbl.setOpaque(true);
+		orderTotalLbl.setPreferredSize(new Dimension(315, 60));
+		orderTotalLbl.setHorizontalAlignment(JLabel.CENTER);
+		//-------------------------------------
+		
+		eastPanel.setBackground(new Color(191, 128, 64));
+		buttonPanel.setBackground(new Color(191, 128, 64));
+		mainPanel.setBackground(new Color(191, 128, 64));
+		buttonPanel.setPreferredSize(new Dimension(320,50));
+		eastPanel.setPreferredSize(new Dimension(350,this.getHeight()));
+		
+		editItem.addActionListener(this);
+		removeItem.addActionListener(this);
+		checkoutButton.addActionListener(this);
+		
+		checkoutButton.setPreferredSize(new Dimension(320, 60));
+		
+		sp.setPreferredSize(new Dimension(320,550));
+
+		buttonPanel.setLayout(new GridLayout(1,2));
+		buttonPanel.add(editItem);
+		buttonPanel.add(removeItem);
+		
+		eastPanel.add(sp);
+		eastPanel.add(buttonPanel);
+		eastPanel.add(orderTotalLbl);
+		eastPanel.add(checkoutButton);
+
+		add(mainPanel, BorderLayout.CENTER);
+		add(eastPanel, BorderLayout.EAST);
 	}
 
 	public static void main(String[] args) {
@@ -38,7 +107,6 @@ public class CoffeeShop extends JFrame implements ActionListener{
 		app.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		app.setTitle("Coffee Shop");
 		app.setVisible(true);
-		//Sneha
 	}
 
 	@Override
