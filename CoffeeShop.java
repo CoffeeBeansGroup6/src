cfd1cfc49d87de5dcd74ae93003d155b8adadfa0
Sarah Taff <sarahtaff@Sarahs-MacBook-Pro-2.local>
2016-12-09 16:46:03 -0600

    receiptIMG

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -712,6 +713,7 @@ public class CoffeeShop extends JFrame implements ActionListener, ItemListener {
 	
 	
 	public void generateReceipt() {
+		
 		//generate receipt
 		ReceiptPanel ReceiptPanel = new ReceiptPanel();
 		JFrame printFrame = new JFrame();
@@ -736,6 +738,8 @@ public class CoffeeShop extends JFrame implements ActionListener, ItemListener {
 	
 	// custom JPanel for on screen receipt
 	class ReceiptPanel extends JPanel implements Printable {
+		Image receiptIMG = Toolkit.getDefaultToolkit().getImage("CoffeeShop.png");
+		ImageIcon recieptIMGICON = new ImageIcon(receiptIMG.getScaledInstance(240,60, Image.SCALE_SMOOTH));
 
 		@Override
 		public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
